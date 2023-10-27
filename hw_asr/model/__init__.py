148eb4f57f71e9cf5b137980a27a645997835db5
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-10-27 19:35:11 +0300

    Update __init__.py

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,11 +1,9 @@
 from hw_asr.model.baseline_model import BaselineModel
 from hw_asr.model.deepspeech2 import DeepSpeech2
-from hw_asr.model.best_model import DeepSpeech_model
-from hw_asr.model.pretrain import DeepSpeech_pretrain
+from hw_asr.model.ds_train import DeepSpeech_big
 
 __all__ = [
     "BaselineModel",
     "DeepSpeech2",
-    "DeepSpeech_model",
-    "DeepSpeech_pretrain"
+    "DeepSpeech_big"
 ]
